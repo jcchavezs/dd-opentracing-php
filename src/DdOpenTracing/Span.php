dd9fe25cf70ac3bd338fa2482f45461bcf2fe782
José Carlos Chávez <jcchavezs@gmail.com>
2017-05-12 10:59:00 +0200

    Addes support for error propagation.

--- src/DdOpenTracing/Span.php
+++ src/DdOpenTracing/Span.php
@@ -2,15 +2,18 @@
 
 namespace DdOpenTracing;
 
+use Exception;
+use InvalidArgumentException;
 use Nanotime\Nanotime;
 use OpenTracing\Exceptions\SpanAlreadyFinished;
 use OpenTracing\Ext\Tags;
-use OpenTracing\Log;
+use OpenTracing\LogField;
 use OpenTracing\Span as OTSpan;
 use DdTrace\Span as DdSpan;
 use OpenTracing\SpanContext;
 use OpenTracing\Tag;
 use OpenTracing\Context as OTContext;
+use Throwable;
 use TracingContext\TracingContext;
 
 final class Span implements OTSpan
@@ -72,7 +75,7 @@ final class Span implements OTSpan
         return SpanContext::create(OTContext::create($context));
     }
 
-    public function finish(Nanotime $finishTimestamp = null)
+    public function finish(Nanotime $finishTimestamp = null, $logRecords = [])
     {
         $this->checkIfSpanIsFinished();
 
@@ -94,12 +97,35 @@ final class Span implements OTSpan
 
         if ($tag->is(Tags::PEER_SERVICE)) {
             $this->span->forService($tag->value());
+        } else if ($tag->is(Tags::ERROR)) {
+            $this->setErrorTag($tag);
         } else {
             $this->span->setMeta($tag->key(), $tag->value());
         }
     }
 
-    public function logFields(Log ...$logs)
+    private function setErrorTag(Tag $tag)
+    {
+        $this->span->setMeta(Tags::ERROR, true);
+
+        if ($tag->value() instanceof Throwable) {
+            $this->span->setError($tag->value());
+            return;
+        }
+
+        if ($tag->value() instanceof Exception) {
+            $this->span->setError($tag->value());
+            return;
+        }
+
+        $tagStrVal = strval($tag->value());
+
+        if ($tagStrVal) {
+            $this->span->setError(new Exception($tagStrVal));
+        }
+    }
+
+    public function logFields(LogField ...$logs)
     {
         $this->checkIfSpanIsFinished();
     }
@@ -117,7 +143,7 @@ final class Span implements OTSpan
     private static function validateOperationName($operationName)
     {
         if ($operationName == '') {
-            throw new \InvalidArgumentException('Empty operation name.');
+            throw new InvalidArgumentException('Empty operation name.');
         }
     }
 
